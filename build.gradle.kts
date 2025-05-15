78202dbccb41c4113d1abd2c43226d05c60c2b7c
cjpoulsen <53880342+cjpoulsen@users.noreply.github.com>
2025-05-15 12:35:23 -0500

    Removed sqids

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.flywaydb:flyway-core")
 	implementation("org.flywaydb:flyway-database-postgresql")
-	implementation("org.sqids:sqids-kotlin:0.1.1")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
